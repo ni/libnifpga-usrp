3180a075e585a47b67479d8b91e0ea9f93eab038
Michael Auchter <michael.auchter@ni.com>
2021-04-19 16:10:08 -0500

    Add missing license headers

--- src/DmaBuf.h
+++ src/DmaBuf.h
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2020 National Instruments
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
 #pragma once
 
 #include "Common.h"
@@ -49,9 +63,7 @@ public:
 
 private:
     explicit DmaBuf(int descriptor, size_t size)
-        : bufFile(descriptor, DeviceFile::ReadWrite)
-        , size(size)
-        , buffer(NULL)
+        : bufFile(descriptor, DeviceFile::ReadWrite), size(size), buffer(NULL)
     {
     }
 
